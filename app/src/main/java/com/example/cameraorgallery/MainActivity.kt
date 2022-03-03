fb7a67217ba53166d664c7d7f7514acbb3b86f62
Yarin Lemos de Alcantara <yarin_alcantara@hotmail.com>
2022-03-03 17:44:59 -0300

    Adicionando comentarios

--- app/src/main/java/com/example/cameraorgallery/MainActivity.kt
+++ app/src/main/java/com/example/cameraorgallery/MainActivity.kt
@@ -13,17 +13,20 @@ import com.example.cameraorgallery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val binding : ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
-    private val getCamera = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+    // Abrir a camera
+    private val getCamera =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
 
-        val bitmap = it?.data?.extras?.get("data") as Bitmap
-        binding.imageView.setImageBitmap(bitmap)
-    }
+            val bitmap = it?.data?.extras?.get("data") as Bitmap
+            binding.imageView.setImageBitmap(bitmap)
+        }
 
+    // Abrir a galeria
     private val getImage = registerForActivityResult(ActivityResultContracts.GetContent(),
         ActivityResultCallback {
-            it?.let{
+            it?.let {
                 binding.imageView.setImageURI(it)
             }
         }
@@ -33,29 +36,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-
-
-
-            with(binding){
-                btEscolherImagem.setOnClickListener {
+        with(binding) {
+            // Ação de clicar o botão e chamada do função do RadioGroup
+            btEscolherImagem.setOnClickListener {
                 ChooseImage()
-                }
             }
         }
-
-    private fun ChooseImage(){
+    }
+ // Função de escolhar a imagem no Radio Button
+    private fun ChooseImage() {
         val dialogEscolha = Dialog(this)
         dialogEscolha.setCancelable(true)
         dialogEscolha.setContentView(R.layout.dialog_image)
-        val radioGroupChoose : RadioGroup = dialogEscolha.findViewById(R.id.rgCameraOrGaleria)
+        val radioGroupChoose: RadioGroup = dialogEscolha.findViewById(R.id.rgCameraOrGaleria)
         radioGroupChoose.setOnCheckedChangeListener { _, i ->
-            when(i){
-                R.id.rbCamera -> {
+            when (i) {
+                R.id.rbCamera -> { // se escolher a opção da camera
                     val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
                     getCamera.launch(intent)
                     dialogEscolha.dismiss()
                 }
-                R.id.rbGaleria -> {
+                R.id.rbGaleria -> { // se escolher a opção da galeria
                     getImage.launch("image/")
                     dialogEscolha.dismiss()
 
@@ -64,5 +65,5 @@ class MainActivity : AppCompatActivity() {
         }
         dialogEscolha.show()
     }
-    }
+}
 
